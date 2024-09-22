97d8211d1c533d2672fdb1751c13ceeaed828441
Vladimir Sokolov <vladimirsokolov1234@gmail.com>
2024-09-23 01:36:38 +0200

    Update Class1.cs

--- Calculator1/Class1.cs
+++ Calculator1/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,6 @@ namespace Calculator1
 {
     public class Calculator
     {
+        int a;
     }
 }
