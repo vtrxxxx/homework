10364d7cd0a601e895575e7fb13e1551f5efc211
Vladimir Sokolov <vladimirsokolov1234@gmail.com>
2024-10-13 17:11:55 +0200

    Lesson4_homeWork

---
+++ Lesson4_HomeWork/Lesson4_HomeWork/Operators.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lesson4_HomeWork
+{
+    internal class Employee
+    {
+        private decimal salary;
+
+        public decimal Salary
+        {
+            get { return salary;}
+            set 
+            {
+                if (value >= 0)
+                    salary = value;
+            }
+        }
+
+        public Employee( decimal salary)
+        {
+            this.Salary = salary;
+        }
+
+        public static Employee operator +(Employee emp, decimal amount)
+        {
+            emp.Salary += amount;
+            return emp;
+        }
+
+        public static Employee operator -(Employee emp, decimal amount)
+        {
+            emp.Salary -= amount;
+            return emp;
+        }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.Salary == emp2.Salary;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return emp1.Salary != emp2.Salary;
+        }
+
+        public static bool operator <(Employee emp1, Employee emp2)
+        {
+            return emp1.Salary < emp2.Salary;
+        }
+
+        public static bool operator >(Employee emp1, Employee emp2)
+        {
+            return emp1.Salary > emp2.Salary;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || !(obj is Employee))
+                return false;
+            return this.Salary == ((Employee)obj).Salary ;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return this.Salary.GetHashCode();
+        }
+
+    }
+
+    public class City
+    {
+        private int population;
+
+        public int Population
+        {
+            get { return population; }
+            set
+            {
+                if (value >= 0)
+                    population = value;
+            }
+        }
+
+        public City( int population)
+        {
+            this.Population = population;
+        }
+
+        public static City operator +(City city, int populationIncrease)
+        {
+            city.Population += populationIncrease;
+            return city;
+        }
+
+        public static City operator -(City city, int populationDecrease)
+        {
+            city.Population -= populationDecrease;
+            return city;
+        }
+
+        public static bool operator ==(City city1, City city2)
+        {
+            return city1.Population == city2.Population;
+        }
+
+        public static bool operator !=(City city1, City city2)
+        {
+            return city1.Population != city2.Population;
+        }
+
+        public static bool operator <(City city1, City city2)
+        {
+            return city1.Population < city2.Population;
+        }
+
+        public static bool operator >(City city1, City city2)
+        {
+            return city1.Population > city2.Population;
+        }
+
+        public override bool Equals(object ?obj)
+        {
+            if (obj == null || !(obj is City))
+                return false;
+            return this.Population == ((City)obj).Population;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Population.GetHashCode();
+        }
+
+    }
+
+    public class CreditCard
+    {
+        private int cvc;
+        private decimal balance;
+
+        public int Cvc { get { return cvc; } set { cvc = value; } }
+        public decimal Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (value >= 0)
+                    balance = value;
+            }
+        }
+        public CreditCard(int cvc, decimal balance)
+        {
+            this.Cvc = cvc;
+            this.Balance = balance;
+        }
+
+        public static CreditCard operator + (CreditCard card , decimal amount)
+        {
+            card.Balance += amount;
+            return card;
+        }
+
+        public static CreditCard operator -(CreditCard card, decimal amount)
+        {
+            card.Balance -= amount;
+            return card;
+        }
+
+        public static bool operator ==(CreditCard card1, CreditCard card2)
+        {
+            return card1.Cvc == card2.Cvc;
+        }
+
+        public static bool operator !=(CreditCard card1, CreditCard card2)
+        {
+            return card1.Cvc != card2.Cvc;
+        }
+
+        public static bool operator <(CreditCard card1, CreditCard card2)
+        {
+            return card1.Balance < card2.Balance;
+        }
+        public static bool operator >(CreditCard card1, CreditCard card2)
+        {
+            return card1.Balance > card2.Balance;
+        }
+
+        public override bool Equals(object? obj)
+        {
+           if (obj == null || !(obj is CreditCard)) return false;
+
+            return this.Cvc == ((CreditCard)obj).Cvc && this.Balance == ((CreditCard)obj).Balance;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Cvc, this.Balance);
+        }
+    }
+
+
+}
---
+++ Lesson4_HomeWork/Lesson4_HomeWork/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Lesson4_HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тестирование класса Employee");
+            Employee emp1 = new Employee(5000);
+            Employee emp2 = new Employee(5500);
+            Employee emp3 = new Employee(5000);
+
+            Console.WriteLine($"emp1 == emp2: {emp1 == emp2}"); 
+            Console.WriteLine($"emp1 == emp3: {emp1 == emp3}"); 
+            Console.WriteLine($"emp1 != emp2: {emp1 != emp2}");
+
+            Console.WriteLine($"emp1.Equals(emp2): {emp1.Equals(emp2)}");
+            Console.WriteLine($"emp1.Equals(emp3): {emp1.Equals(emp3)}");
+
+            emp1 += 500;
+            Console.WriteLine($"Зарплата emp1 после увеличения: {emp1.Salary}"); 
+            emp1 -= 1000;
+            Console.WriteLine($"Зарплата emp1 после уменьшения: {emp1.Salary}"); 
+
+            Console.WriteLine($"emp1 > emp2: {emp1 > emp2}"); 
+            Console.WriteLine($"emp1 < emp2: {emp1 < emp2}");  
+
+            Console.WriteLine("\nТестирование класса City");
+           
+            City city1 = new City(2800000);
+            City city2 = new City(720000);
+            City city3 = new City(2800000);
+
+            Console.WriteLine($"city1 == city2: {city1 == city2}"); 
+            Console.WriteLine($"city1 == city3: {city1 == city3}"); 
+            Console.WriteLine($"city1 != city2: {city1 != city2}"); 
+
+            Console.WriteLine($"city1.Equals(city2): {city1.Equals(city2)}"); 
+            Console.WriteLine($"city1.Equals(city3): {city1.Equals(city3)}"); 
+
+            city1 += 50000;
+            Console.WriteLine($"Население city1 после увеличения: {city1.Population}"); 
+            city1 -= 100000;
+            Console.WriteLine($"Население city1 после уменьшения: {city1.Population}");
+
+            Console.WriteLine($"city1 > city2: {city1 > city2}"); 
+            Console.WriteLine($"city1 < city2: {city1 < city2}"); 
+
+            Console.WriteLine("\nТестирование класса CreditCard");
+
+            CreditCard card1 = new CreditCard(123, 1000m);
+            CreditCard card2 = new CreditCard(456, 1500m);
+            CreditCard card3 = new CreditCard(123, 1000m);
+
+            Console.WriteLine($"card1 == card2: {card1 == card2}"); 
+            Console.WriteLine($"card1 == card3: {card1 == card3}"); 
+            Console.WriteLine($"card1 != card2: {card1 != card2}");
+
+            Console.WriteLine($"card1.Equals(card2): {card1.Equals(card2)}"); 
+            Console.WriteLine($"card1.Equals(card3): {card1.Equals(card3)}");
+
+            card1 += 200m;
+            Console.WriteLine($"Баланс card1 после увеличения: {card1.Balance}");
+            card1 -= 300m;
+            Console.WriteLine($"Баланс card1 после уменьшения: {card1.Balance}");
+
+            Console.WriteLine($"card1 > card2: {card1 > card2}");
+            Console.WriteLine($"card1 < card2: {card1 < card2}");
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
