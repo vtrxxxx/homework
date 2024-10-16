a1fbaf53f587367d0ea5bedf79ceeb5a9c1368ae
Vladimir Sokolov <vladimirsokolov1234@gmail.com>
2024-10-16 18:42:23 +0200

    lesson7_homework

---
+++ homework_lesson5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace homework_lesson5
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            using (Play play1 = new Play("Гамлет", "Вільям Шекспір", "Трагедія", 1600))
+            {
+                play1.DisplayInfo();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Shop shop1 = new Shop("Продуктовий Рай", "вул. Центральна, 10", "Продовольчий");
+            shop1.DisplayInfo();
+            GC.Collect();
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
